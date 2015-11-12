d3811305fa4c270a9e3acf698e2889af611c2267
135yshr <isago@praisefirst.jp>
2015-11-12 23:04:11 +0900

    refactor to testcase

--- point_test.go
+++ point_test.go
@@ -9,23 +9,26 @@ import (
 
 var _ = Describe("Point", func() {
 	Describe("平面座標上の点", func() {
+		var p Point
 		Context("xに1 yに1を渡したとき", func() {
+			BeforeEach(func() {
+				p = Point{X: 1, Y: 1}
+			})
 			It("xが1になること", func() {
-				p := Point{X: 1, Y: 1}
 				Expect(p.X).To(Equal(1))
 			})
 			It("yが1になること", func() {
-				p := Point{X: 1, Y: 1}
 				Expect(p.Y).To(Equal(1))
 			})
 		})
 		Context("xに2 yに3を渡したとき", func() {
+			BeforeEach(func() {
+				p = Point{X: 2, Y: 3}
+			})
 			It("xが2になること", func() {
-				p := Point{X: 2, Y: 3}
 				Expect(p.X).To(Equal(2))
 			})
 			It("yが3になること", func() {
-				p := Point{X: 2, Y: 3}
 				Expect(p.Y).To(Equal(3))
 			})
 		})
