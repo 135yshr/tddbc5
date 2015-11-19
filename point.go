a6e716aa5f257fd280ae3e60584a55711a22d506
135yshr <isago@praisefirst.jp>
2015-11-19 20:42:46 +0900

    mod HorizontalOf ot Hide

--- point.go
+++ point.go
@@ -21,13 +21,13 @@ func (g *GridPoint) NeighborOf(other GridPoint) bool {
 	if g.SameCoordinatesWith(other) {
 		return false
 	}
-	return g.verticalOf(other) && g.Y == other.Y || g.HorizontalOf(other) && g.X == other.X
+	return g.verticalOf(other) && g.Y == other.Y || g.horizontalOf(other) && g.X == other.X
 }
 
 func (g *GridPoint) verticalOf(o GridPoint) bool {
 	return (g.X == o.X-1 || g.X == o.X+1)
 }
 
-func (g *GridPoint) HorizontalOf(o GridPoint) bool {
+func (g *GridPoint) horizontalOf(o GridPoint) bool {
 	return (g.Y == o.Y-1 || g.Y == o.Y+1)
 }
