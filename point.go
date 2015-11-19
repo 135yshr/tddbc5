7394144b338d444d66fd7f0e4d3eea25e41b5f4d
135yshr <isago@praisefirst.jp>
2015-11-19 19:44:14 +0900

    add testcase Notation -2,3

--- point.go
+++ point.go
@@ -1,10 +1,14 @@
 package tddbc5
 
+import (
+	"fmt"
+)
+
 type GridPoint struct {
 	X int
 	Y int
 }
 
 func (g *GridPoint) Notation() string {
-	return "(4,7)"
+	return fmt.Sprintf("(%d,%d)", g.X, g.Y)
 }
--- point_test.go
+++ point_test.go
@@ -9,10 +9,16 @@ import (
 
 var _ = Describe("格子点", func() {
 	Describe("格子点を文字列で表す", func() {
-		Context("座標Xが4、座標Yが7の格子点を作成する", func() {
+		Context("座標Xが4、座標Yが7の格子点を作成したとき", func() {
 			It("文字列表記が(4,7)になること", func() {
 				sut := GridPoint{4, 7}
 				Expect(sut.Notation()).To(Equal("(4,7)"))
+			})
+		})
+		Context("座標Xが-2、座標Yが3の格子点を作成したとき", func() {
+			It("文字列表記が(-2,3)になること", func() {
+				sut := GridPoint{-2, 3}
+				Expect(sut.Notation()).To(Equal("(-2,3)"))
 			})
 		})
 	})
